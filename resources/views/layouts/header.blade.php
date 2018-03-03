0498f573665886d8a17fa7fec68528405c3042b8
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-03-03 08:50:04 +0100

    creating header

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>SSIA</title>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+  </head>
+
+  <body>
+    <div id = 'wrapper'>
+      <header>
+        <div class = 'headerImage'>
+          <img src= "{{ asset('images/header.jpg')}}">
+        </div>
+
+        <div class = 'logo cfx'>
+          <img src= "{{ asset('images/logo.jpg')}}">
+        </div>
+
+        <div class = 'headerTitle'>
+          <h1>SSIA</h1>
+        </div>
+      </header>
+
+      <nav>
+        <ul class = 'cfx'>
+          <li><a href = '#'>Home</a></li>
+          <li><a href = '#'>Seksuele intimidatie</a></li>
+          <li><a href = '#'>Getuigenissen</a></li>
+          <li><a href = '#'>Media</a></li>
+          <li><a href = '#'>Profiel</a></li>
+        </ul>
+      </nav>
+    </div>
+  </body>
+</html>
