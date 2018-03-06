e4db6a60054c67cd61d5bf4f3521cd93f77b8736
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-03-06 16:38:33 +0100

    add "wat is seksuele intimidatie" page

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,4 @@
 Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/wat-is-seksuele-intimidatie', 'WatIsSeksueleIntimidatieController@index')->name('WatIsSeksueleIntimidatie');
