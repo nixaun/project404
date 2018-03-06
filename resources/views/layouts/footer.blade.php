4c0bc4b0f036fad4bcfdb201574a1d3987491a11
nixaun <lenn_peeters@yahoo.com>
2018-03-06 16:23:13 +0100

    basic footer update

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,5 +1,26 @@
-  <footer>
+<footer class="wrapper">
+  <div>
+    <ul>
+      <h2>Snel zelf regelen</h2>
+      <li><a href="#">Documenten aanvragen</a></li>
+      <li><a href="#">Sluikstort melden</a></li>
+      <li><a href="#">Adreswijziging doorgeven</a></li>
+      <li><a href="#">Parkeerverbod aanvragen</a></li>
+    </ul>
+  </div>
+  <div>
+    <ul>
+      <h2>Dienstverlening</h2>
+      <li><a href="#">E-loket</a></li>
+      <li><a href="#">Afspraak</a></li>
+      <li><a href="#">Melding maken</a></li>
+      <li><a href="#">Openingsuren</a></li>
+    </ul>
+  </div>
+  <div>
+    <h2>Contact met stad Antwerpen</h2>
+    <h2>Schrijf u in op onze nieuwsbrief</h2>
+    <h2>Vind ons op</h2>
+  </div>
 
-
-  </footer>
-</html>
+</footer>
