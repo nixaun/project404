2a25e6f39cd80cef8ca7958231e88513acd7b714
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-04-19 09:59:39 +0200

    add Wat is seksuele intimidatie pages

---
+++ resources/views/hoeKanIkAangifteDoen.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.default')
+
+@section('content')
+<div class='wrapper'>
+  <div class="sidenav cfx">
+    <a href="#">Wat is seksuele intimidatie?</a>
+    <a href="#">Welk gedrag is strafbaar?</a>
+    <a href="#">Hoe kan ik aangifte doen?</a>
+  </div>
+
+  <div class = 'aside cfx'>
+    <div class="content-split">
+      <h1>Hoe kan ik aangifte doen?</h1>
+      <h3>Er wordt maar zelden klacht ingediend wegens seksuele intimidatie. Slachtoffers verdringen vaak wat er gebeurd is en dienen niet snel klacht in. Nochtans is aangifte of melding doen een belangrijke en vaak noodzakelijke stap. Zo kan de politie in geval van een misdrijf een onderzoek starten naar de daders en overlast op straat doelgericht aanpakken.</h3>
+      <p>Aangifte doen kan op verschillende manieren.</p>
+      <ul>
+        <li>Bij strafbare feiten en voor dringende politiehulp bel onmiddellijk het noodnummer 101.</li>
+        <li>Voor niet-dringende hulp:</li>
+        <ul>
+          <li>Bel de Blauwe Lijn 0800 123 12.</li>
+          <li>Maak een <a href = 'https://www.politieantwerpen.be/afspraak' target="_blank">online afspraak met Politie Antwerpen.</a><li>
+        </ul>
+        <li>Bij het<a href = '' target="_blank"> Instituut voor de Gelijkheid van Vrouwen en Mannen.</a><li>
+      </ul>
+    </div>
+  </div>
+</div>
+@endsection
---
+++ resources/views/welkGedragIsStrafbaar.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.default')
+
+@section('content')
+<div class='wrapper'>
+  <div class="sidenav cfx">
+    <a href="#">Wat is seksuele intimidatie?</a>
+    <a href="#">Welk gedrag is strafbaar?</a>
+    <a href="#">Hoe kan ik aangifte doen?</a>
+  </div>
+
+  <div class = 'aside cfx'>
+      <div class="content-split">
+        <h1>Welk gedrag is strafbaar?</h1>
+        <h3>Ook al voelt niet iedereen zich even snel seksueel geïntimideerd, toch is ongewenst gedrag in bepaalde gevallen strafbaar.</h3>
+        <p>U kan persoonlijk aangifte doen, maar de politie kan ook zelf een vaststelling doen en een proces verbaal opmaken zonder dat er een klacht is ingediend. Het gaat hier om ongewenst, storend, intimiderend of choquerend gedrag dat seksueel getint of geladen kan zijn en waarbij het slachtoffer zich in zijn rust gestoord voelt.</p>
+        <p>Voor dit soort strafbaar gedrag spreekt de wet over:</p>
+        <ul>
+          <li>Belaging (Strafwetboek Art.442 bis): Alle gedrag dat de rust van de getroffen persoon ernstig verstoort zoals bijvoorbeeld achternalopen, omringen, intimiderende of beledigende sms-en of berichten op facebook sturen enzovoort.</li>
+          <li>Voyeurisme (Strafwetboek Art.371): Personen observeren of doen observeren, maar ook beelden of geluidsopnamen van iemand maken, zonder dat hij/zij dat weet of daarvoor toestemming heeft gegeven.</li>
+        </ul>
+        <p>Als dit gedrag wordt gesteld ten opzichte van minderjarigen, geldt dat als verzwarende omstandigheid en zijn ook de straffen zwaarder.</p>
+        <p>Bij ernstigere feiten, bijvoorbeeld bij geweld of bedreigingen, waarbij het slachtoffer zich voelt aangetast in zijn seksuele integriteit, is er sprake van <a href = 'https://www.allesoverseks.be/themas/seks-grenzen/over-je-grens/wat-is-aanranding-van-de-eerbaarheid' target="_blank">aanranding van de eerbaarheid.</a></p>
+      </div>
+
+      <div class = 'content-split'>
+        <h2>Meer info over de wetgeving omtrent seksisme en seksuele intimidatie?</h2>
+        <a href = 'https://www.allesoverseks.be/themas/seks-grenzen/seks-de-wet' target="_blank">Sensoa: Seks en de wet</a>
+      </div>
+  </div>
+</div>
+@endsection
