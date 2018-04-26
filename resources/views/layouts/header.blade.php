f06b3e7b35a429b2a6e199783be1fb00fa72b64f
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-04-26 14:58:35 +0200

    change login file

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -71,6 +71,21 @@
           <li><a href = '#'></a></li>
           <li><a href = '#'></a></li>
         </ul>
+
+        @if(Auth::user())
+          <a href="{{ route('logout') }}"
+             onclick="event.preventDefault();
+                           document.getElementById('logout-form').submit();">
+              Logout
+          </a>
+          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+              @csrf
+          </form>
+        @endif
+
+        @if ((!Auth::user()))
+          <a href = "{{route ('login')}}">Login</a>
+        @endif
       </nav>
 
     </div>
