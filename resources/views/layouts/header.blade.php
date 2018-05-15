b46cecc318b4c2873425786453e817e24411b6f7
RubensVdM <rubenvdm@telenet.be>
2018-05-15 14:21:15 +0200

    Header redone

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,10 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>SSIA</title>
+    <title>STOP_SEKSUELE_INTIMIDATIE</title>
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
@@ -16,63 +18,29 @@
 
   <body>
     <div id = 'header'>
-      <header>
-        <div class = 'headerImage'>
-          <img src= "{{ asset('images/hero-banner-1.jpg')}}">
-        </div>
 
-        <div class = 'logo cfx'>
-          <img src= "{{ asset('images/logo.jpg')}}">
-        </div>
+        <img src="{{ asset('images/BORD2.png')}}" alt="BORD2">
 
-        <div class = 'headerTitle'>
-          <h1>Filler</h1>
-        </div>
-      </header>
+    <nav>
+      <div class="P1">
+      <ul>
+            <li><a href="{{route('WatIsSeksueleIntimidatie')}}"></a>Seksuele Intimidatie</li>
+            <li><a href="{{route('Verhalen')}}"></a>Verhalen</li>
+      </ul>
+      </div>
 
-      <div id='navbar-line'>
-
-      <div class="CRNT_TAB">
-        <a href="{{route('home')}}"><img src="{{ asset('images/HOME2.png')}}" alt="HOME"></a>
-        <a href="{{route('WatIsSeksueleIntimidatie')}}"><img src="{{ asset('images/SI.png')}}" alt="SI"></a>
-        <a href="{{route('Verhalen')}}"><img src="{{ asset('images/VERHALEN.png')}}" alt="VERHALEN"></a>
-        <a href="{{route('WatKanUZelfDoen')}}"><img src="{{ asset('images/WAT.png')}}" alt="WAT KAN U ZELF DOEN?"></a>
-        <a href="{{route('Media')}}"><img src="{{ asset('images/MEDIA.png')}}" alt="MEDIA"></a>
-        <a href="{{route('Profiel')}}"><img src="{{ asset('images/PROFIEL.png')}}" alt="PROFIEL"></a>
+      <div class="P2">
+        <ul>
+              <li><a href="#"></a><i class="fas fa-user fa-2x"></i></li>
+              <li><a href="#"></a>Media</li>
+              <li><a href="{{route('WatKanUZelfDoen')}}"></a>SOS</li>
+        </ul>
+      </div>
+    </nav>
 
       </div>
 
-      </div>
-<!--
       <nav>
-        <ul class = 'cfx wrapper'>
-          <li><a href = "{{route('home')}}">Home</a></li>
-          <li><a href = "{{route('WatIsSeksueleIntimidatie')}}">Seksuele intimidatie</a></li>
-          <li><a href = '#'>Verhalen</a></li>
-          <li><a href = "{{route('WatKanUZelfDoen')}}">Wat kan u zelf doen?</a></li>
-          <li><a href = '#'>Media</a></li>
-          <li><a href = '#'>Profiel</a></li>
-        </ul>
-        <a href="{{ route('logout') }}"
-           onclick="event.preventDefault();
-                         document.getElementById('logout-form').submit();">
-            Logout
-        </a>
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-            @csrf
-        </form>
-      </nav>
--->
-      <nav>
-        <ul class = 'cfx wrapper'>
-          <li><a href = "{{route('home')}}"></a></li>
-          <li><a href = "{{route('WatIsSeksueleIntimidatie')}}"></a></li>
-          <li><a href = "{{route('Verhalen')}}"></a></li>
-          <li><a href = "{{route('WatKanUZelfDoen')}}"></a></li>
-          <li><a href = '#'></a></li>
-          <li><a href = '#'></a></li>
-        </ul>
-
         @if(Auth::guard('web')->check() || Auth::guard('admin')->check())
           <a href="{{ route('logout') }}"
              onclick="event.preventDefault();
