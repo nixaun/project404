ac4c972035a9baddb7c88783ab436e694c7e6f25
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-05-17 11:11:12 +0200

    update homepagina

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,15 +3,33 @@
 @section('content')
   <div class="wrapper">
     <div class="content-split content-home">
-      <h1>Random Filmpje</h1>
-      <h3>Social Experiment</h3>
-      <iframe width="720px" height="405px" src="https://www.youtube.com/embed/9uccExOPMrI" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+      <h1>Media</h1>
+      @foreach ($media as $video)
+        @if ($video->id == $randomArray1[$random1])
+            @if($video->isAnonymous == 'on')
+              <h2>Anoniem</h2>
+            @else
+              <h2>{{ $video->user->firstname}}</h2>
+            @endif
+            <h3>{{ $video->title}}</h3>
+            <iframe width="720px" height="405px" src='{{$video->url}}' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen class="media-iframe"></iframe>
+          @endif
+        @endforeach
     </div>
+
     <div class="content-split content-home">
-      <h1>Random Verhaal</h1>
-      <h3>Eva (22)</h3>
-      <p>Ik was met de tram onderweg naar Hoboken om mij in te schrijven op de hogeschool. Ik zat op een enkel zitje tegen het raam. Het rijtuig zat vol, maar staanplaatsen waren er nog genoeg. Een man kwam tegen mijn zitje staan, met zijn geslachtsdeel tegen mijn arm. Als toch nog wel ‘klein meisje in een grote stad’ durfde ik niet te bewegen en heb ik de volle vijf minuten (die wel uren leken te duren) naar buiten gestaard. Na mij ging hij naar het volgende meisje en deed hij exact hetzelfde.</p>
-      <p>Ook kan ik mij enorm kwaad maken in jongens die op een feestje willen passeren en me onnodig vastnemen in mijn zij of aan mijn heupen. Ik ben er zéker van dat veel meisjes deze situatie zullen herkennen. Het is écht niet moeilijk om gewoon te passeren zonder ons lichaam aan te raken, laat staan vast te nemen. Het ergste vind ik dat ze niet eens beseffen dat dit fout is, en dan ook ‘kwaad’ worden als hen raar bekijkt of hun handen wegduwt.</p>
+      <h1>Verhaal</h1>
+      @foreach ($verhalen as $verhaal)
+        @if ($verhaal->id == $randomArray[$random])
+            @if($verhaal->isAnonymous == 'on')
+              <h2>Anoniem</h2>
+            @else
+              <h2>{{ $verhaal->user->firstname}}</h2>
+            @endif
+            <h3>{{ $verhaal->title}}</h3>
+            <p>{{$verhaal->body}}</p>
+          @endif
+        @endforeach
     </div>
     <div class="content-split content-home">
       <h1>Onze Awesome Game</h1>
