afdbf26e4f1647a724e81fbd7ed7e2b4d6739cd4
nixaun <lenn_peeters@yahoo.com>
2018-05-24 14:27:26 +0200

    styling profiel page

--- resources/views/profiel.blade.php
+++ resources/views/profiel.blade.php
@@ -6,12 +6,14 @@
       <h1 class="form-header">Profiel</h1>
       @foreach($user as $profile)
         @if(Auth::user()->name == $profile->name)
+        <div class="form-container">
           <h3>Naam: {{$profile->name}}</h3>
           <h3>Voornaam: {{$profile->firstname}}</h3>
           <h3>Geboortedatum: </h3>
           <h3>Gebruikersnaam: {{$profile->username}}</h3>
           <h3>E-mail: {{$profile->email}}</h3>
           <a class="button-profile" href='/profiel/wijzigen/{{$profile->id}}'>Profiel wijzigen</a>
+        </div>
         @endif
       @endforeach
     </div>
