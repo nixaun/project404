9bcd86aea2d119405c54a82baf3aeb49e7512173
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-06-09 16:30:15 +0200

    add sql

--- routes/web.php
+++ routes/web.php
@@ -11,38 +11,40 @@
 |
 */
 
-Auth::routes();
+Route::group(['middleware' => ['web']], function() {
+  Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
-Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/wat-is-seksuele-intimidatie', 'WatIsSeksueleIntimidatieController@index')->name('WatIsSeksueleIntimidatie');
-Route::get('/verhalen', 'VerhaalsController@index')->name('Verhalen');
-Route::get('/media', 'MediaController@index')->name('Media');
-Route::get('/wat-kan-u-zelf-doen', 'WatKanUZelfDoenController@index')->name('WatKanUZelfDoen');
-Route::get('/profiel', 'ProfielController@index')->name('Profiel');
+  Route::get('/', 'HomeController@index')->name('home');
+  Route::get('/home', 'HomeController@index')->name('home');
+  Route::get('/wat-is-seksuele-intimidatie', 'WatIsSeksueleIntimidatieController@index')->name('WatIsSeksueleIntimidatie');
+  Route::get('/verhalen', 'VerhaalsController@index')->name('Verhalen');
+  Route::get('/media', 'MediaController@index')->name('Media');
+  Route::get('/wat-kan-u-zelf-doen', 'WatKanUZelfDoenController@index')->name('WatKanUZelfDoen');
+  Route::get('/profiel', 'ProfielController@index')->name('Profiel');
 
-Route::get('/verhalen/toevoegen', 'VerhaalsController@addVerhaal')->middleware('auth')->name('addVerhaal');
-Route::post('/verhalen/invoegen', 'VerhaalsController@insertVerhaal')->middleware('auth')->name('insertVerhaal');
-Route::get('verhalen/wijzigen/{id}', 'VerhaalsController@editVerhaal')->middleware('auth');
-Route::post('verhalen/wijzigen/{id}', 'VerhaalsController@updateVerhaal')->middleware('auth');
-Route::post('verhalen/goedkeuren/{id}', 'VerhaalsController@goedkeuren');
-Route::post('verhalen/verwijderen/bevestigen/{id}', 'VerhaalsController@delete');
-Route::get('verhalen/verwijderen/{id}', 'VerhaalsController@showConfirm');
-Route::post('verhalen', 'VerhaalsController@index');
+  Route::get('/verhalen/toevoegen', 'VerhaalsController@addVerhaal')->middleware('auth')->name('addVerhaal');
+  Route::post('/verhalen/invoegen', 'VerhaalsController@insertVerhaal')->middleware('auth')->name('insertVerhaal');
+  Route::get('verhalen/wijzigen/{id}', 'VerhaalsController@editVerhaal')->middleware('auth');
+  Route::post('verhalen/wijzigen/{id}', 'VerhaalsController@updateVerhaal')->middleware('auth');
+  Route::post('verhalen/goedkeuren/{id}', 'VerhaalsController@goedkeuren');
+  Route::post('verhalen/verwijderen/bevestigen/{id}', 'VerhaalsController@delete');
+  Route::get('verhalen/verwijderen/{id}', 'VerhaalsController@showConfirm');
+  Route::post('verhalen', 'VerhaalsController@index');
 
-Route::get('/media/toevoegen', 'MediaController@addMedia')->middleware('auth')->name('addMedia');
-Route::post('/media/invoegen', 'MediaController@insertMedia')->middleware('auth')->name('insertMedia');
-Route::get('media/wijzigen/{id}', 'MediaController@editMedia')->middleware('auth');
-Route::post('media/wijzigen/{id}', 'MediaController@updateMedia')->middleware('auth');
-Route::post('media/goedkeuren/{id}', 'MediaController@goedkeuren');
-Route::post('media/verwijderen/bevestigen/{id}', 'MediaController@delete');
-Route::get('media/verwijderen/{id}', 'MediaController@showConfirm');
-Route::post('media', 'MediaController@index');
+  Route::get('/media/toevoegen', 'MediaController@addMedia')->middleware('auth')->name('addMedia');
+  Route::post('/media/invoegen', 'MediaController@insertMedia')->middleware('auth')->name('insertMedia');
+  Route::get('media/wijzigen/{id}', 'MediaController@editMedia')->middleware('auth');
+  Route::post('media/wijzigen/{id}', 'MediaController@updateMedia')->middleware('auth');
+  Route::post('media/goedkeuren/{id}', 'MediaController@goedkeuren');
+  Route::post('media/verwijderen/bevestigen/{id}', 'MediaController@delete');
+  Route::get('media/verwijderen/{id}', 'MediaController@showConfirm');
+  Route::post('media', 'MediaController@index');
 
-Route::get('/profiel/wijzigen/{id}', 'ProfielController@editProfiel')->middleware('auth');
-Route::post('/profiel/wijzigen/{id}', 'ProfielController@updateProfiel')->middleware('auth');
+  Route::get('/profiel/wijzigen/{id}', 'ProfielController@editProfiel')->middleware('auth');
+  Route::post('/profiel/wijzigen/{id}', 'ProfielController@updateProfiel')->middleware('auth');
 
-Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
-Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
+  Route::get('/admin/login', 'Auth\AdminLoginController@showLoginForm')->name('admin.login');
+  Route::post('/admin/login', 'Auth\AdminLoginController@login')->name('admin.login.submit');
 
-Route::get('/game', 'GameController@index')->name('Game');
+  Route::get('/game', 'GameController@index')->name('Game');
+}); 
