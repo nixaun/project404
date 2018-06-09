4190a963b9f48162ed00c5fd66a5a243b93f1c82
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-06-09 18:22:59 +0200

    change link footer

--- app/User.php
+++ app/User.php
@@ -31,9 +31,4 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
-
-    public function username()
-    {
-       return 'username';
-    }
 }
