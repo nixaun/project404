ca1570e59216656611eabcfa972b6bdf15b8eedb
LoreVdl <32160288+LoreVdl@users.noreply.github.com>
2018-06-10 23:19:24 +0200

    hamburger bug fixed

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'StopSeksueleIntimidatie@gmail.com'),
-        'name' => env('MAIL_FROM_NAME', 'StopSeksueleIntimidatie'),
+        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
     /*
