dcc0affa24dafe36551976abbd18e0dd8439b00a
nixaun <lenn_peeters@yahoo.com>
2018-06-10 20:37:53 +0200

    email layout

--- resources/views/emails/register.blade.php
+++ resources/views/emails/register.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome Email</title>
+    <title>Bevestiging registratie bij Stop Sekuele Intimidatie Antwerpen.</title>
 </head>
 
 <body>
-<h2>Welcome to the site</h2>
+<h2>Bedankt voor het aanmaken van een account.</h2>
 <br/>
-Your registered email-id is
+We hopen dat je correct gebruik zal maken van al de functionaliteiten op onze website.
+Moest je nog vragen hebben, dan kan je altijd terecht op onze site of partnersites.
+
+Met vriendelijke groeten,
+het SSIA team
 </body>
 
 </html>
